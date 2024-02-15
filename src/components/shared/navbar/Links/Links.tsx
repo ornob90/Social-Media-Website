05c118c12f7c17843b4f21e5c3e13e45565cd239
ornob90 <oornob49@gmail.com>
2024-02-15 16:30:16 +0600

    Add active and hover style in the leftsidebar links in Home page

--- src/components/shared/navbar/Links/Links.tsx
+++ src/components/shared/navbar/Links/Links.tsx
@@ -2,7 +2,7 @@
 import { LinkType } from "@/types/global.types";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 type Links = {
   mobile?: boolean;
@@ -13,23 +13,42 @@ type Links = {
 
 const Links = ({ mobile, setMenuOpen, navLinks, flexCol }: Links) => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
+  const activeSideNav = searchParams.get("active-side-nav");
   return (
     <ul
       className={`${
         mobile
           ? "flex gap-3   items-end px-[10%]"
-          : "justify-end items-center hidden gap-4 md:flex "
-      } ${flexCol ? "flex-col" : ""}`}
+          : "justify-end items-center hidden  md:flex "
+      } ${flexCol ? "flex-col gap-2" : "gap-4"}`}
     >
       {navLinks?.map((link) => (
-        <li key={link.src}>
+        <li key={link.src} className={`${flexCol ? "w-full" : ""} `}>
           <Link
             href={link.href}
-            className={`flex items-center ${
-              pathname === link.href ? "text-dark font-bold" : "text-light"
+            className={` flex items-center ${
+              pathname === link.href
+                ? "text-dark font-bold rounded-[8px]"
+                : "text-light"
             }
-          `}
+            ${
+              flexCol
+                ? "flex gap-2 hover:bg-light-gray hover:rounded-[14px] duration-300 px-4 py-3 border-b"
+                : ""
+            }
+            ${
+              flexCol && activeSideNav === link?.name?.toLocaleLowerCase()
+                ? "bg-light-gray rounded-[14px]"
+                : ""
+            }
+            ${
+              flexCol && pathname === link?.href && !activeSideNav
+                ? "bg-light-gray rounded-[14px]"
+                : ""
+            }
+            `}
             onClick={() => mobile && setMenuOpen?.(false)}
           >
             <Image
@@ -38,7 +57,9 @@ const Links = ({ mobile, setMenuOpen, navLinks, flexCol }: Links) => {
               width={link.width}
               height={link.height}
             />
-            {link.showName && link.name}
+            {link.showName && (
+              <p className="font-semibold text-md">{link.name}</p>
+            )}
           </Link>
         </li>
       ))}
--- src/components/ui/home/LeftNavigation/LeftNavigation.tsx
+++ src/components/ui/home/LeftNavigation/LeftNavigation.tsx
@@ -1,7 +1,13 @@
+import Links from "@/components/shared/navbar/Links/Links";
+import { sideHomeLinks } from "@/data/navLinks";
 import React from "react";
 
 const LeftNavigation = () => {
-  return <aside className="col-span-2 min-h-[400px] h-screen bg-white"></aside>;
+  return (
+    <aside className="col-span-2 min-h-[400px] h-screen bg-white py-4 px-2 rounded-md">
+      <Links navLinks={sideHomeLinks} flexCol />
+    </aside>
+  );
 };
 
 export default LeftNavigation;
