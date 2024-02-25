ba6041f5f8c45af7152451e2761ea8db55266700
ornob90 <oornob49@gmail.com>
2024-02-25 11:00:16 +0600

    Add user model structure

---
+++ src/server/models/user.ts
@@ -0,0 +1,43 @@
+import { Document, Schema, model, models } from "mongoose";
+
+interface IUser extends Document {
+  userName: string;
+  displayName: string;
+  password: string;
+  email: string;
+  photoUrl?: string;
+  phone?: string;
+  bio?: string;
+}
+
+const userSchema: Schema = new Schema<IUser>({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  photoUrl: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  bio: {
+    type: String,
+    maxLength: 150,
+  },
+});
+
+export const User = models.User || model<IUser>("User", userSchema);
